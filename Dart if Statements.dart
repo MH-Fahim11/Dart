3d7b20cd0cefdb88f937b341761ed26f8b515e9c
Mahmudul Hasan Fahim <95010186+MH-Fahim11@users.noreply.github.com>
2021-11-28 12:27:25 +0600

    Create Dart if Statements.dart

---
+++ Dart if Statements.dart
@@ -0,0 +1,9 @@
+void main () {  
+  // define a variable which hold numeric value  
+ var n = 35;  
+   
+ // if statement check the given condition  
+ if (n<40){  
+    print("The number is smaller than 40")  
+ };  
+}
