a1cb2f8eea7c95a8408d2d278bfe7857fe1b2678
Mahmudul Hasan Fahim <95010186+MH-Fahim11@users.noreply.github.com>
2021-11-28 19:53:32 +0600

    Create Dart for loop.dart

---
+++ Dart for loop.dart
@@ -0,0 +1,8 @@
+void main()  
+{  
+    int num = 1;  
+    for(num; num<=10; num++)           //for loop to print 1-10 numbers  
+    {  
+        print(num);     //to print the number  
+    }  
+}  
