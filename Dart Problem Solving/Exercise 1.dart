26de2c8760c1d7050aa91ff733618b3afe83dcd8
Mahmudul Hasan Fahim <95010186+MH-Fahim11@users.noreply.github.com>
2021-11-29 22:35:20 +0600

    Update Exercise 1.dart

--- Dart Problem Solving/Exercise 1.dart
+++ Dart Problem Solving/Exercise 1.dart
@@ -1,1 +1,9 @@
+import 'dart:io';
 
+main() {
+  stdout.writeln('What is your name?');
+  var name = stdin.readLineSync();
+  stdout.writeln('How Old Are You?');
+  int age = stdin.readLineSync() as int;
+  print('$name, You have ${100 - age} years to be 100');
+}
