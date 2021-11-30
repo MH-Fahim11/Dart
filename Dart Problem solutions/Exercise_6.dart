288851fc44f0d819cac52d7e75f3b6597878efa4
Mahmudul Hasan Fahim <95010186+MH-Fahim11@users.noreply.github.com>
2021-11-30 11:29:44 +0600

    Update Exercise_6.dart

--- Dart Problem solutions/Exercise_6.dart
+++ Dart Problem solutions/Exercise_6.dart
@@ -1,1 +1,16 @@
+import 'dart:io';
 
+void main() {
+  
+  stdout.write("Please give a word: ");
+  String input = stdin.readLineSync()!.toLowerCase();
+  String revInput = input.split('').reversed.join('');
+  
+  if(input == revInput){
+       print("The word is palindrome");
+      }
+  else {
+       print("The word is not a palindrome");
+  }
+      
+}
